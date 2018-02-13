ee1653c2b76928446db754d2cc31903d270e56b7
DawidStefaniak <contact@dawidstefaniak.com>
2018-02-13 14:32:11 +0000

    android core added

---
+++ RestaurantBilling.Droid/Setup.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Platform;
+using RestaurantBilling.Core;
+
+
+namespace RestaurantBilling.Droid
+{
+    public class Setup : MvxAndroidSetup
+    {
+        protected Setup(Context applicationContext) : base(applicationContext)
+        {
+
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
