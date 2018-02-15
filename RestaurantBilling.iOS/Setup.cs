c358d2b33c7e0b4b397348c5eda51f08b998b3fd
DawidStefaniak <contact@dawidstefaniak.com>
2018-02-15 08:52:30 +0000

    Added iOS app without views

---
+++ RestaurantBilling.iOS/Setup.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using RestaurantBilling.Core;
+using UIKit;
+
+namespace RestaurantBilling.iOS
+{
+    class Setup : MvxIosSetup
+    {
+        public Setup(IMvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter) : 
+            base(applicationDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
